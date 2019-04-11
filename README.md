9d798e8f53f5f51a2520523e37438ff49fc68a3b
Nathan Craddock <nzcraddock@gmail.com>
2019-04-10 21:26:06 -0600

    Update Readme Small tweaks to code Added image

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# dr-epilepsy
+# Dr. Epilepsy
 A 2.80 port of the classic Blender addon. Dr Epilepsy will melt your brain
+
+### [Download Here]()
+
+![Brain Melt!](brain_melt.png)
+
+Install the addon, press CTRL+D in the 3D View, or search for Dr Epilepsy in the search menu (F3).
+
+Enjoy!
+
+Please do not use if you are sensitive to bright flashing colors.
--- dr_epilepsy.py
+++ dr_epilepsy.py
@@ -16,18 +16,15 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
-# <pep8 compliant>
-
 bl_info = {
     'name': 'Dr. Epilepsy',
     'author': 'Nathan Craddock, Bassam Kurdali, Dalai Felinto, Nathan Letwory, inspired by Bastian Salmela',
-    'version': (1, 1, 0),
+    'version': (1, 1),
     "blender": (2, 80, 0),
-    "api": 39104,
-    'location': 'View3D > OSKey-D',
-    'description': 'Dr. Epililepsy will melt your brain',
-    'wiki_url': '',
-    'tracker_url': '',
+    'location': '3D View > CTRL-D',
+    'description': 'Dr. Epilepsy will melt your brain',
+    'wiki_url': 'https://github.com/natecraddock/dr-epilepsy',
+    'tracker_url': 'https://github.com/natecraddock/dr-epilepsy/issues',
     'category': '3D View'
 }
 
@@ -38,17 +35,16 @@ from mathutils import Color
 from math import sin
 import aud
 import math
-import bgl
 
 
-def parseNotes(notes, bpm, basefreq, rate = 44100,
-               notechars = "XXXCXDXEFXGXAXHcXdXefXgXaXhp"):
+def parseNotes(notes, bpm, basefreq, rate=44100,
+               notechars="XXXCXDXEFXGXAXHcXdXefXgXaXhp"):
     pos = 0
     sound = None
     fadelength = 60/bpm/10
     halfchars = "#b"
     durationchars = "2345678"
- 
+
     while pos < len(notes):
         char = notes[pos]
         mod = None
@@ -60,16 +56,16 @@ def parseNotes(notes, bpm, basefreq, rate = 44100,
             elif notes[pos] in durationchars:
                 dur = notes[pos]
             pos += 1
- 
+
         freq = notechars.find(char)
         if mod == '#':
             freq += 1
         elif mod == 'b':
             freq -= 1
- 
+
         freq = math.pow(2, freq/12)*basefreq
         length = float(dur)*60/bpm
- 
+
         snd = aud.Sound.sine(freq, rate)
         if char == 'p':
             snd = snd.volume(0)
@@ -78,42 +74,45 @@ def parseNotes(notes, bpm, basefreq, rate = 44100,
         snd = snd.limit(0, length)
         snd = snd.fadein(0, fadelength)
         snd = snd.fadeout(length - fadelength, fadelength)
- 
+
         if sound:
             sound = sound.join(snd)
         else:
             sound = snd
     return sound
- 
-def tetris(bpm = 300, freq = 220, rate = 44100):
+
+
+def tetris(bpm=300, freq=220, rate=44100):
     notes = "e2Hcd2cH A2Ace2dc H3cd2e2 c2A2A4 pd2fa2gf e3ce2dc H2Hcd2e2 c2A2A2p2"
     s11 = parseNotes(notes, bpm, freq, rate)
- 
+
     notes = "e4c4 d4H4 c4A4 G#4p4 e4c4 d4H4 A2c2a4 g#4p4"
     s12 = parseNotes(notes, bpm, freq, rate)
- 
+
     notes = "EeEeEeEe AaAaAaAa AbabAbabAbabAbab AaAaAAHC DdDdDdDd CcCcCcCc HhHhHhHh AaAaA2p2"
-    s21 = parseNotes(notes, bpm, freq, rate, notechars = "AXHCXDXEFXGXaXhcXdXefXgXp")
- 
+    s21 = parseNotes(notes, bpm, freq, rate, notechars="AXHCXDXEFXGXaXhcXdXefXgXp")
+
     notes = "aeaeaeae g#dg#dg#dg#d aeaeaeae g#dg#dg#2p2 aeaeaeae g#dg#dg#dg#d aeaeaeae g#dg#dg#2p2"
     s22 = parseNotes(notes, bpm, freq/2, rate)
- 
+
     return s11.join(s12).join(s11).volume(0.5).mix(s21.join(s22).join(s21).volume(0.3))
- 
-def play(bpm = 300, freq = 220):
+
+
+def play(bpm=300, freq=220):
     dev = aud.Device()
-    h= dev.play(tetris(bpm, freq, dev.rate))
+    h = dev.play(tetris(bpm, freq, dev.rate))
     h.loop_count = -1
     return h
 
-class BLENDER_OT_dr_epilepsy(bpy.types.Operator):
-    '''Makes the UI beautiful while singing a delightful tune to increase productivity.'''
+
+class VIEW_3D_OT_dr_epilepsy(bpy.types.Operator):
+    '''Makes the UI beautiful while singing a delightful tune to increase productivity...'''
     bl_idname = "wm.drepilepsy"
     bl_label = "Dr Epilepsy"
 
     _timer = None
     _handle = None
-    _pattern =  0b0011001110011001
+    _pattern = 0b0011001110011001
 
     def modal(self, context, event):
         if event.type == 'ESC':
@@ -125,39 +124,42 @@ class BLENDER_OT_dr_epilepsy(bpy.types.Operator):
                     for space in area.spaces:
                         if space.type == 'VIEW_3D':
                             space.overlay.grid_scale = 3 + 2*sin(4*time.time())
-            
+
             # change theme color, silly!
             theme = context.preferences.themes[0]
             for buttin in dir(theme.user_interface):
-                button = getattr(theme.user_interface,buttin)
+                button = getattr(theme.user_interface, buttin)
                 if type(button) == bpy.types.ThemeWidgetColors:
                     for element in dir(button):
-                        if element in ['outline','item','inner','inner_sel']:
-                            elem = getattr(button,element)
+                        if element in ['outline', 'item', 'inner', 'inner_sel']:
+                            elem = getattr(button, element)
                             elem[0] = random()
                             elem[1] = random()
                             elem[2] = random()
-            context.preferences.view.ui_scale = 1 + random()*2
+            
+            context.preferences.view.ui_scale = 1 + (random() * 3)
+            
             for vb in dir(theme):
-                bov = getattr(theme,vb)
-                cols = [thing for thing in dir(bov) if type(getattr(bov,thing))==Color]
+                bov = getattr(theme, vb)
+
+                # Color buttons and other elements
+                cols = [thing for thing in dir(bov) if type(getattr(bov, thing)) == Color]
                 for col in cols:
-                    color = getattr(bov,col)
-                    
+                    color = getattr(bov, col)
                     color.r = random()
                     color.g = random()
                     color.b = random()
-                
+
                 if hasattr(bov, "space"):
                     space = bov.space
-                    
+
                     # Color the back of each area
                     if hasattr(space, "back"):
                         back = space.back
                         back.r = random()
                         back.g = random()
                         back.b = random()
-                    
+
                     # Color each part of the space
                     parts = [part for part in dir(space) if type(getattr(space, part)) is bpy.types.bpy_prop_array]
                     for part in parts:
@@ -165,7 +167,7 @@ class BLENDER_OT_dr_epilepsy(bpy.types.Operator):
                         color[0] = random()
                         color[1] = random()
                         color[2] = random()
-                    
+
                     # Color the panels
                     if hasattr(space, "panelcolors"):
                         panel = bov.space.panelcolors
@@ -181,7 +183,7 @@ class BLENDER_OT_dr_epilepsy(bpy.types.Operator):
             background_color[0] = random()
             background_color[1] = random()
             background_color[2] = random()
-            
+
         return {'PASS_THROUGH'}
 
     def execute(self, context):
@@ -197,13 +199,14 @@ class BLENDER_OT_dr_epilepsy(bpy.types.Operator):
 
 
 def register():
-    bpy.utils.register_class(BLENDER_OT_dr_epilepsy)
+    bpy.utils.register_class(VIEW_3D_OT_dr_epilepsy)
     kc = bpy.context.window_manager.keyconfigs.addon
     km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
-    kmi = km.keymap_items.new('wm.drepilepsy', 'D', 'PRESS', oskey=True)
+    kmi = km.keymap_items.new('wm.drepilepsy', 'D', 'PRESS', ctrl=True)
+
 
 def unregister():
-    bpy.utils.unregister_class(BLENDER_OT_dr_epilepsy)
+    bpy.utils.unregister_class(VIEW_3D_OT_dr_epilepsy)
     km = bpy.context.window_manager.keyconfigs.addon.keymaps["3D View"]
     for kmi in km.keymap_items:
         km.keymap_items.remove(kmi)
